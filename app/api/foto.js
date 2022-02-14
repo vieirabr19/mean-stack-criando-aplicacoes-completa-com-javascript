19c9cd24c67b38e377a72dbbb315ef86b8ee61fc
Luiz Vieira <luiz22_sp@hotmail.com>
2022-02-14 20:40:01 -0300

    05 faça sua escolha e conheça o Curinga

--- app/api/foto.js
+++ app/api/foto.js
@@ -1,12 +1,20 @@
 var api = {};
 
-api.lista = function(req, res) {
-  var photos = [
-    {_id: 1, titulo: 'Photo 1', url: 'https://www.infoescola.com/wp-content/uploads/2017/04/leao-126767138-1000x673.jpg'},
-    {_id: 2, titulo: 'Photo 2', url: 'https://www.infoescola.com/wp-content/uploads/2017/04/leao-413856229-1000x667.jpg'}
-  ];
+var photos = [
+  {_id: 1, titulo: 'Photo 1', url: 'https://www.infoescola.com/wp-content/uploads/2017/04/leao-126767138-1000x673.jpg'},
+  {_id: 2, titulo: 'Photo 2', url: 'https://www.infoescola.com/wp-content/uploads/2017/04/leao-413856229-1000x667.jpg'}
+];
 
+api.lista = function(req, res) {
   res.json(photos);
+}
+
+api.fotoPorId = function(req, res){
+  var foto = photos.find(function(foto){
+    return foto._id == req.params.id;
+  });
+
+  res.json(foto);
 }
 
 module.exports = api;
--- app/routes/foto.js
+++ app/routes/foto.js
@@ -1,4 +1,5 @@
 module.exports = function(app){
   var api = app.api.foto;
   app.get('/v1/fotos', api.lista);
+  app.get('/v1/fotos/:id', api.fotoPorId);
 }
