65cd496fb8a43ec08f9bbdf55b552803ee6465d1
Luiz Vieira <luiz22_sp@hotmail.com>
2022-02-18 07:30:29 -0300

    09 cordão umbilical servidor e banco

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 var http = require('http');
 var app = require('./config/express');
 
+require('./config/database')('localhost/mean-stack-criando-aplicacoes-completa-com-javascript');
+
 http.createServer(app).listen(3000, function() {
-  console.log('Servidor iniciado');
+  console.log('Servidor iniciado na porta 3000');
 });
 
 // http.createServer(function(req, res){
