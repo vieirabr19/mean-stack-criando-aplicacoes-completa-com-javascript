c484b00366fac5ca80b78e01cfb0239dd48026d1
Luiz Vieira <luiz22_sp@hotmail.com>
2022-02-25 20:30:36 -0300

    10 qual é o esquema

--- app/api/foto.js
+++ app/api/foto.js
@@ -1,48 +1,51 @@
-var api = {};
+var mongoose = require('mongoose');
+var model = mongoose.model('Foto');
 
-var GENERATE_ID = 2;
+module.exports = function(app){
+  var api = {};
+  
+  api.lista = function(req, res) {
+    model
+      .find({})
+      .then(function(fotos) {
+        res.json(fotos);
+      }, function(error) {
+        console.log('Erro',error);
+        res.status(500).json(error);
+      });
+  }
+  
+  api.fotoPorId = function(req, res){
+    // var foto = photos.find(function(foto){
+    //   return foto._id == req.params.id;
+    // });
+    // res.json(foto);
+  }
+  
+  api.removePorId = function(req, res){
+    // photos = photos.filter(function(foto){
+    //   return foto._id != req.params.id;
+    // });
+    // res.sendStatus(204);
+  }
+  
+  api.adiciona = function(req, res){
+    // const foto = req.body;
+    // foto._id = ++GENERATE_ID;
+    // photos.push(foto);
+    // res.json(foto);
+  }
+  
+  api.atualiza = function(req, res){
+    // var foto = req.body;
+    // var fotoId = req.params.id;
+    // var indice = photos.findIndex(function(foto){
+    //   return foto._id == fotoId;
+    // });
+  
+    // photos[indice] = foto;
+    // res.sendStatus(200);
+  }
 
-var photos = [
-  {_id: 1, titulo: 'Photo 1', url: 'https://www.infoescola.com/wp-content/uploads/2017/04/leao-126767138-1000x673.jpg'},
-  {_id: 2, titulo: 'Photo 2', url: 'https://www.infoescola.com/wp-content/uploads/2017/04/leao-413856229-1000x667.jpg'}
-];
-
-api.lista = function(req, res) {
-  res.json(photos);
+  return api;
 }
-
-api.fotoPorId = function(req, res){
-  var foto = photos.find(function(foto){
-    return foto._id == req.params.id;
-  });
-
-  res.json(foto);
-}
-
-api.removePorId = function(req, res){
-  photos = photos.filter(function(foto){
-    return foto._id != req.params.id;
-  });
-
-  res.sendStatus(204);
-}
-
-api.adiciona = function(req, res){
-  const foto = req.body;
-  foto._id = ++GENERATE_ID;
-  photos.push(foto);
-  res.json(foto);
-}
-
-api.atualiza = function(req, res){
-  var foto = req.body;
-  var fotoId = req.params.id;
-  var indice = photos.findIndex(function(foto){
-    return foto._id == fotoId;
-  });
-
-  photos[indice] = foto;
-  res.sendStatus(200);
-}
-
-module.exports = api;
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var app = require('./config/express');
 
-require('./config/database')('localhost/mean-stack-criando-aplicacoes-completa-com-javascript');
+require('./config/database')('localhost/alurapic');
 
 http.createServer(app).listen(3000, function() {
   console.log('Servidor iniciado na porta 3000');
